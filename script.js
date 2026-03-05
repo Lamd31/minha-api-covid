cde1189bc0a71df881f2fe57f3f81b4202de0bef
Daniel03 <danielmarcosaraujo@gmail.com>
2026-03-04 21:30:32 -0300

    criado comparcao entre estados api covid no js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+const inputEstado1 = document.querySelector("#estado1");
+const inputEstado2 = document.querySelector("#estado2");
+const btnBuscar = document.querySelector("#btnBuscar");
+// const resultado3 = document.querySelector("#resultado3");
 
-function encontrarEstadoApi(){
+
+btnBuscar.addEventListener("click", ()=> {
+    const nomeEstado1 = inputEstado1.value.trim();
+    const nomeEstado2 = inputEstado2.value.trim();
+    encontrarDados1(nomeEstado1,nomeEstado2); 
+    
+})
+
+
+
+function encontrarStatusApi(){
     
     fetch(`https://covid19-brazil-api.vercel.app/api/status/v1`)
     .then(response => response.json())
@@ -13,12 +27,73 @@ function encontrarEstadoApi(){
     })
     .catch(err => console.error(err));
     
-}
+};
+encontrarStatusApi();
 
-encontrarEstadoApi();
+
+
+
+
+function encontrarDados1(nomeEstado1,nomeEstado2){
+    console.log(nomeEstado1,nomeEstado2)
+    
+    fetch(`https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${nomeEstado1}`)
+    .then(resposta => resposta.json())
+    .then(dados => {
+        // console.log(dados)
+        const estadoApi1 = dados.uf
+        const morteApi1 = dados.deaths
+        // console.log(estadoApi1,morteApi1)
+        exibirCasos(estadoApi1,morteApi1)
+    })
+    
+    
+    fetch(`https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${nomeEstado2}`)
+    .then(resposta => resposta.json())
+    .then(dados => {
+        // console.log(dados)
+        const estadoApi2 = dados.uf
+        const morteApi2 = dados.deaths
+        // console.log(estadoApi2,morteApi2)
+         exibirCasos(estadoApi2,morteApi2)
+    })
+    exibirCasos();
+};
+
+// function encontrarDados2(){
+    
+// };
+// encontrarDados1();
+// encontrarDados2();
+
+
+
+// function mediaEstados(){
+//     if(morteApi1>morteApi2){
+//         resultado1.innerHTML = ``
+//     }
+// };
+
+
+
 
 function exibirEstado(busca){
     const statusApi = document.querySelector("#statusApi");
     console.log(busca)
     busca==1?    statusApi.innerHTML = `Online` : statusApi.innerHTML =`Offline`
+};
+
+function exibirCasos(estadoApi1, morteApi1){
+    estadoAtual = ""
+    morteAtual = 0
+    const resultado1 = document.querySelector("#resultado1");
+    if(morteApi1>morteAtual){
+        morteAtual=morteApi1 
+        estadoAtual=estadoApi1
+    }    
+    resultado1.innerHTML = `O ${estadoAtual} teve ${morteAtual} mortes`
 }
+
+// function exibirCasos2( estadoApi2, morteApi2){
+//     resultado2.innerHTML = `${estadoApi2} ${morteApi2}`
+// }
